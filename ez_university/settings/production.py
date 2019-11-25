862eac72de2c960f44822ee378a344c4c5bd2971
Luna <yyx444312799@gmail.com>
2019-11-24 20:55:55 -0600

    add production link

--- ez_university/settings/production.py
+++ ez_university/settings/production.py
@@ -3,4 +3,4 @@ from .base import *
 DEBUG = False
 
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "trainor1.pythonanywhere.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "yanxing2.pythonanywhere.com"]
